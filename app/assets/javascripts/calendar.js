a7ca8e2915a89793b9779c0f4b7eb483e60cf945
TND23 <tommyn@isomedia.com>
2015-04-03 16:19:27 -0700

    Calendar is approximately working

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -10,43 +10,105 @@ $(document).ready(function() {
     defaultView: 'month',
     height: 500,
     slotMinutes: 60,
-
+    ignoreTimezone: false,
     eventSources: [
       {
         // magic number which is liable to change if routes get moved around
         // this is built this way so users can look at each others calendars
         // since now the data will fetch json data from the url in the window
         url: '/users/' +  window.location.pathname.split("/")[2] + '/activities',
-        color: 'yellow',
+        color: '#3B91AD',
         textColor: 'black'
       }
     ],
     // when rendering, the elements title will display the tags
     eventRender: function(event, element, view) {
+      //console.log(event);
       return element.attr({
         "title": this.tag
       });
     },
+
+    eventClick: function(event){
+      // blank out the background screen
+      $("#overlay").show();
+      $("#hiddenForm").show();
+
+      renderForm(event);
+      return false;
+    },
+
     timeFormat: 'h:mm t{ - h:mm t} ',
     dragOpacity: "0.5",
     eventDrop: function(activity, dayDelta, minuteDelta, allDay, revertFunc) {
-
       return updateEvent(activity);
     },
+
     eventResize: function(activity, dayDelta, minuteDelta, revertFunc) {
-      // sendAjaxRequest(activity);
+      // sendAjaxRequest(activity);;
       return updateEvent(activity);
     }
   });
 });
 
 updateEvent = function(the_activity) {
-  return $.update("/activies/" + the_activity.id, {
-    activity: {
-      title: the_activity.title,
-      starts_at: "" + the_activity.started,
-      ends_at: "" + the_activity.finished,
-      description: the_activity.description
-    }
+  //debugger;
+  // javascript assumes that + is trying to concat the string. 
+  // to get around this, we substract the negative.
+  // the_activity._start = the_activity.start = new Date(the_activity.start - (delta * -1));
+  // the_activity._end = the_activity.end = new Date(the_activity.end - (delta * -1));
+  // debugger;
+  $.ajax({
+    type: 'PUT',
+    dataType: 'json',
+    url: the_activity.url,
+    data: {
+      activity: {
+        content: the_activity.long_description, 
+        tag: the_activity.tag, 
+        status: the_activity.current_state, 
+        overview: the_activity.title, 
+        started: the_activity.start, 
+        finished: the_activity.end 
+      }, _method : 'put'
+    },
+    error: function(data,resp,error){
+      console.log(resp); console.log(error);
+    },
+    success: function(){ console.log("SUCCESS"); }
   });
+
+};
+
+renderForm = function(activity){
+  var hiddenForm = document.getElementById("hidden-form");
+  hiddenForm.style.display = "block";
+  var form = generateForm(activity, listenToClick);
+  return false;
+};
+
+generateForm = function(activity, callback){
+  // nasty
+  var f = document.getElementById("temp-form");
+
+  var i1 = document.getElementById("activity_tag")
+  var i2 = document.getElementById("activity_overview");
+  var i3 = document.getElementById("activity_content");
+  i1.setAttribute('value', activity.tag);
+  i2.setAttribute('value', activity.title);
+  i3.setAttribute('value', activity.long_description);
+
+  document.getElementById("hidden-form").appendChild(f);
+  callback(activity);
+};
+
+listenToClick = function(activity){
+  // take current form values and set them to whatever.
+  $("#submit-hidden-form").click(function(){
+    activity.tag = document.getElementById("activity_tag").value;
+    activity.overview = document.getElementById("activity_overview").value;
+    activity.content = document.getElementById("activity_content").value;
+    activity.status = document.getElementById("activity_status").value;
+    updateEvent(activity);
+  })
 };
